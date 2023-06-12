b9daf230d32f4b19cf7ba12f84d2c6d18c8a6f41
PhTrias <vettrias@outlook.com>
2023-06-12 11:04:12 -0300

    feat: faker gem and propertie validations

--- Gemfile
+++ Gemfile
@@ -32,6 +32,7 @@ gem "bootsnap", require: false
 
 gem 'kaminari'
 gem 'active_model_serializers'
+gem 'faker'
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
--- app/models/propertie.rb
+++ app/models/propertie.rb
@@ -1,2 +1,12 @@
 class Propertie < ApplicationRecord
+  validates :business_type, presence: true
+  validates :value, presence: true
+  validates :size, presence: true
+  validates :address, presence: true
+  validates :cep, presence: true
+  validates :neighborhood, presence: true
+
+  def full_price_sum
+    self.full_price = iptu + condominium + value
+  end
 end
