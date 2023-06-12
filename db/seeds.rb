ed2c2fa8d758ab996778495c25185c51782cb98a
PhTrias <vettrias@outlook.com>
2023-06-12 11:03:46 -0300

    feat: create properties in rails seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+50.times do
+  Propertie.create(
+    business_type: %w[rent adm].sample,
+    value: rand(2000..4000),
+    iptu: rand(400..700),
+    condominium: rand(200..500),
+    size: rand(80..130),
+    rooms: rand(1..4),
+    zone: %w[north west south east].sample,
+    address: Faker::Address.street_name,
+    number: Faker::Address.building_number,
+    neighborhood: "Bairro",
+    complement: %w[Casa Apartamento].sample,
+    cep: Faker::Address.zip_code
+  )
+end
+
