36b1671d97591fc2f3d936d5be9d21c0bef89221
PhTrias <vettrias@outlook.com>
2023-06-25 15:19:17 -0300

    feat: Create whatsapp messaging with twilio

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,47 @@
+class ContactsController < ApplicationController
+  include Helpers::WhatsappMessaging
+
+  before_action :initialize_messaging_client, only: :create
+  before_action :set_propertie, only: :create, if: -> { contact_params[:propertie_id].presence }
+
+  def create
+    if send_whatsapp_message != 201
+      render json: { successful: true }, status: 200
+    else
+      resnder json: { successful: false }, status: 422
+    end
+  end
+
+  private
+
+  def contact_params
+    params.permit(
+      :name,
+      :phone,
+      :business_type,
+      :message_type,
+      :propertie_id
+    )
+  end
+
+  def set_propertie
+    @propertie = Propertie.find(contact_params[:propertie_id])
+
+    render json: { error: I18n.t('api.properties.not_found') }, status: 404 unless @propertie.present?
+  end
+
+  def initialize_messaging_client
+    @client = Twilio::REST::Client.new(ENV.fetch('TWILIO_ACCOUNT_SID', ''), ENV.fetch('TWILIO_AUTH_TOKEN', ''))
+  end
+
+  # Bussines -> customer contact
+  def send_business_whatsapp_message
+    response = @client.message.create(
+      body: send(contact_params[:message_type]),
+      from: "whatsapp:#{ENV['COMPANY_WHATSAPP_CONTACT']}",
+      to: "whatsapp:#{contact_params[:phone]}"
+    )
+
+    response.code
+  end
+end
---
+++ app/helpers/whatsapp_messaging.rb
@@ -0,0 +1,9 @@
+module WhatsappMessaging
+  def propertie_interest_messaging
+    "Olá, Somos da Ni-imóveis! Vimos que você teve interesse em alugar o imóvel com o endreço: #{@propertie.address}. Como posso te ajudar?"
+  end
+
+  def interest_messaging
+    "Olá, Somos da Ni-imóveis! Vimos que você teve interesse em alugar um imóvel! Como posso te ajudar?"
+  end
+end
