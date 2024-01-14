20f468428248e1fec5106b6234fb0c4c64160049
PhTrias <vettrias@outlook.com>
2024-01-13 21:19:36 -0300

    fix: comment wpp messag sender

--- app/controllers/api/contacts_controller.rb
+++ app/controllers/api/contacts_controller.rb
@@ -2,17 +2,16 @@ class Api::ContactsController < Api::ApplicationController
   include Authorization::Basic
   include WhatsappMessaging
 
-  before_action :authorize!
+  before_action :authenticate!
   before_action :set_propertie, only: :create, if: -> { contact_params[:propertie_id].presence }
 
   def create
-    send_whatsapp_message
+    # send_whatsapp_message
 
-    binding.pry
     if @service == true
       render json: { successful: true }, status: 200
     else
-      resnder json: { successful: false }, status: 422
+      render json: { successful: false }, status: 422
     end
   end
 
@@ -33,10 +32,9 @@ class Api::ContactsController < Api::ApplicationController
     render json: { error: I18n.t('api.properties.not_found') }, status: 404 unless @propertie.present?
   end
 
-  def send_whatsapp_message
-    binding.pry
-    contact_params.merge!({ propertie: @propertie.as_json }) if @propertie.present?
-
-    @service = Whatsapp::Service.new(contact_params).send_message
-  end
+  # def send_whatsapp_message
+  #   contact_params.merge!({ propertie: @propertie.as_json }) if @propertie.present?
+#
+  #   @service = Whatsapp::Service.new(contact_params).send_message
+  # end
 end
