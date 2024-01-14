184d7e760f4d638fe78852ee25e31ae5540c513f
PhTrias <vettrias@outlook.com>
2024-01-13 21:47:56 -0300

    feat: add properties filters

--- app/controllers/api/properties_controller.rb
+++ app/controllers/api/properties_controller.rb
@@ -5,7 +5,20 @@ class Api::PropertiesController < Api::ApplicationController
   before_action :set_propertie, only: %i[show edit update]
 
   def index
-    @properties = Propertie.all
+    @properties = Propertie
+      .by_business_type(params[:business_type])
+      .by_value(params[:value])
+      .by_iptu(params[:iptu])
+      .by_condominium(params[:condominium])
+      .by_size(params[:size])
+      .by_rooms(params[:rooms])
+      .by_zone(params[:zone])
+      .by_address(params[:address])
+      .by_number(params[:number])
+      .by_neighborhood(params[:neighborhood])
+      .by_complement(params[:complement])
+      .by_cep(params[:cep])
+      .by_full_price(params[:full_price])
 
     render(
       json: @properties,
@@ -21,7 +34,11 @@ class Api::PropertiesController < Api::ApplicationController
 
   def update
     if @propertie.update(propertie_params)
-      render json: :ok
+      render(
+        json: @propertie,
+        adapter: :json,
+        serializer: PropertieSerializer,
+      )
     else
       render json: :unprocessable_entity
     end
--- app/models/propertie.rb
+++ app/models/propertie.rb
@@ -1,6 +1,6 @@
 class Propertie < ApplicationRecord
   serialize :pictures, Array
-  
+
   validates :business_type, presence: true
   validates :value, presence: true
   validates :size, presence: true
@@ -23,4 +23,18 @@ class Propertie < ApplicationRecord
 
     self.full_price = calculate_full_price
   end
+
+  scope :by_business_type, ->(business_type) { where(business_type: business_type) if business_type.present? }
+  scope :by_value, ->(value) { where(value: value) if value.present? }
+  scope :by_iptu, ->(iptu) { where(iptu: iptu) if iptu.present? }
+  scope :by_condominium, ->(condominium) { where(condominium: condominium) if condominium.present? }
+  scope :by_size, ->(size) { where(size: size) if size.present? }
+  scope :by_rooms, ->(rooms) { where(rooms: rooms) if rooms.present? }
+  scope :by_zone, ->(zone) { where(zone: zone) if zone.present? }
+  scope :by_address, ->(address) { where(address: address) if address.present? }
+  scope :by_number, ->(number) { where(number: number) if number.present? }
+  scope :by_neighborhood, ->(neighborhood) { where(neighborhood: neighborhood) if neighborhood.present? }
+  scope :by_complement, ->(complement) { where(complement: complement) if complement.present? }
+  scope :by_cep, ->(cep) { where(cep: cep) if cep.present? }
+  scope :by_full_price, ->(full_price) { where(full_price: full_price) if full_price.present? }
 end
