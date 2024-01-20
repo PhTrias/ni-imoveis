b231bd679e907c585f777211a4307237d274f7bd
PhTrias <vettrias@outlook.com>
2024-01-20 14:36:49 -0300

    feat: add enabling aws trigger

--- config/initializers/aws_sdk.rb
+++ config/initializers/aws_sdk.rb
@@ -1,6 +1,9 @@
-Aws.config.update(
-  {
-    region: ENV["AWS_REGION"],
-    credentials: Aws::Credentials.new(ENV["AWS_KEY"], ENV["AWS_SECRET"])
-  }
-)
+if ENV["USE_AWS_INTEGRATION"].present?
+  return unless ENV["USE_AWS_INTEGRATION"] == true
+  Aws.config.update(
+    {
+      region: ENV["AWS_REGION"],
+      credentials: Aws::Credentials.new(ENV["AWS_KEY"], ENV["AWS_SECRET"])
+    }
+  )
+end
