7687ea27d31df5ac7ad27c9237ffc3014ae615d4
PhTrias <vettrias@outlook.com>
2024-01-20 14:41:30 -0300

    fix: remove sdk

--- config/initializers/aws_sdk.rb
+++
@@ -1,9 +0,0 @@
-if ENV["USE_AWS_INTEGRATION"].present?
-  return unless ENV["USE_AWS_INTEGRATION"] == true
-  Aws.config.update(
-    {
-      region: ENV["AWS_REGION"],
-      credentials: Aws::Credentials.new(ENV["AWS_KEY"], ENV["AWS_SECRET"])
-    }
-  )
-end
